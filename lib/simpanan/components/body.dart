7397c14174e0228115da6db30b22d7c46990ade8
maulanaputraaa <maulanaputra2003@gmail.com>
2024-08-20 17:03:07 +0700

    simpanan page

--- lib/simpanan/components/body.dart
+++ lib/simpanan/components/body.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mpay_godong/simpanan/components/simpanan_menu.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'This is the body of SimpananScreen',
-        style: TextStyle(fontSize: 24),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SimpananMenu(),
+          const SizedBox(height: 20),
+        ],
       ),
     );
   }
--- lib/simpanan/components/simpanan_menu.dart
+++ lib/simpanan/components/simpanan_menu.dart
@@ -3,28 +3,135 @@ import 'package:flutter/material.dart';
 class SimpananMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              // Aksi menu 1
-            },
-            child: Text('Menu 1'),
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.9,
+              minWidth: 100, // Lebar minimal
+            ),
+            padding: const EdgeInsets.all(36),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 6,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Transaksi : 0',
+                  style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.04, fontWeight: FontWeight.bold, color: Colors.grey),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        'Setoran : Rp.0',
+                        style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.04, color: Colors.grey),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Penarikan : Rp.0',
+                        style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.04, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              // Aksi menu 2
+          const SizedBox(height: 30),
+          LayoutBuilder(
+            builder: (context, constraints) {
+              double screenWidth = MediaQuery.of(context).size.width;
+              double fontSize = screenWidth * 0.05;
+
+              return Text(
+                'Menu Tabungan',
+                style: TextStyle(fontSize: fontSize, color: Colors.grey),
+              );
             },
-            child: Text('Menu 2'),
           ),
-          ElevatedButton(
-            onPressed: () {
-              // Aksi menu 3
-            },
-            child: Text('Menu 3'),
+          const SizedBox(height: 20),
+
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              crossAxisSpacing: 16.0,
+              mainAxisSpacing: 16.0,
+              children: <Widget>[
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Aksi menu 1
+                  },
+                  child: const Text('Menu 1'),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Aksi menu 2
+                  },
+                  child: const Text('Menu 2'),
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              crossAxisSpacing: 16.0,
+              mainAxisSpacing: 16.0,
+              children: <Widget>[
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Aksi menu 1
+                  },
+                  child: const Text('Menu 3'),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Aksi menu 2
+                  },
+                  child: const Text('Menu 4'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
